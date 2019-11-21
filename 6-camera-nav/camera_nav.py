8114e9e2e1f03e2a1b00ac4cbdebdb98bdd69e11
Danny Staple <danny@orionrobots.co.uk>
2019-11-21 20:52:26 +0000

    Part 6 - camera navigation. Driving by colour.

---
+++ 6-camera-nav/camera_nav.py
@@ -0,0 +1,20 @@
+import gpiozero
+from camera_setup import setup_camera
+from find_contours import get_saturated_contours
+
+robot = gpiozero.Robot(left=(27, 17), right=(24, 23))
+camera, capture_buffer = setup_camera()
+
+for raw in camera.capture_continuous(capture_buffer, format="bgr"):
+    image = raw.array
+    masked, contours, found_colour = get_saturated_contours(image)
+    print(f"Colour {found_colour}, h value: {found_colour[0]}")
+    if 5 < found_colour[0] < 40:
+        print("yellow")
+        robot.left()
+    elif 100 < found_colour < 135:
+        print("blue")
+        robot.right()
+    else:
+        robot.forward()
+    capture_buffer.truncate(0)
---
+++ 6-camera-nav/find_contours.py
@@ -0,0 +1,49 @@
+import time
+import imutils
+import numpy as np
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+
+def setup_camera():
+    camera = PiCamera()
+    camera.resolution = (128, 128)
+    camera.rotation = 180
+
+    capture_buffer = PiRGBArray(camera, size=(128, 128))
+    camera.start_preview()
+    time.sleep(2)
+    return camera, capture_buffer
+
+
+def get_saturated_contours(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Mask for vivid colours
+    masked = cv2.inRange(hsv, np.array([0, 140, 30]), np.array([255, 255, 255]))
+    # Find Contours
+    cnts = cv2.findContours(masked.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(cnts)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    colour = [0, 0, 0]
+    if len(contours) > 0:
+        # Find the center of the contour
+        m = cv2.moments(contours[0])
+        if m["m00"] > 0:
+            cx = int(m["m10"] / m["m00"])
+            cy = int(m["m01"] / m["m00"])
+            colour = hsv[cx, cy]
+    return masked, contours, colour
+
+
+if __name__ == '__main__':
+    camera, capture_buffer = setup_camera()
+    camera.capture(capture_buffer, format="bgr")
+    image = capture_buffer.array
+    masked, contours, found_colour = get_saturated_contours(image)
+    cv2.imwrite('original.png', image)
+    cv2.imwrite('masked.png', masked)
+    cv2.drawContours(image, contours[:1], -1, (0, 255, 0), 1)
+    cv2.imwrite('with_contours.png', image)
+    print(found_colour)
